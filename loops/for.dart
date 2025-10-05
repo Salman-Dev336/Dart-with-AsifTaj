39b69b9d21adf10ec84524e62f3f27c6d6b6b95a
Salman-Dev336 <salmantechhub336@gmail.com>
2025-10-05 11:10:10 -0700

    even odd by using for loop

---
+++ loops/for.dart
@@ -0,0 +1,10 @@
+void main() {
+  for (int i = 1; i <= 50; i++) {
+    if (i % 2 == 0) {
+      print('$i is even number');
+    } else {
+      print('$i is an odd number');
+    }
+  }
+  print('end of loop');
+}
