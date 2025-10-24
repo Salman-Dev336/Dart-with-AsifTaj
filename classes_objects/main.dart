ba54f79b54899b35c1b4c8cd4a381054164f1c06
Salman-Dev336 <salmantechhub336@gmail.com>
2025-10-25 01:45:52 +0500

    null safety

--- classes_objects/main.dart
+++ classes_objects/main.dart
@@ -1,21 +1,27 @@
 class Person {
   String? name;
-  int age = 22;
-  String country = 'pakistan';
+  int? age;
+  String? country;
+  bool? isStudent;
 
   void display() {
-    print('Name: $name, Age: $age, Country: $country');
+    print('Name: $name, Age: $age, Country: $country , is Student: $isStudent');
   }
 }
 
 void main() {
   Person person = Person();
   Person person2 = Person();
+
   person.name = 'salman';
+  person.age = 22;
+  person.country = 'pakistan';
+  person.isStudent = true;
 
   person2.name = 'mashal';
   person2.age = 30;
   person2.country = 'india';
+  person2.isStudent = false;
 
   person.display();
   person2.display();
